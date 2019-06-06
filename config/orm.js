ec2342a8d584aa8f8f39c178bbb9f3b5f3dbb51d
Kevin Bevil <kbevil@gmail.com>
2019-06-06 19:36:38 -0400

    Progress made in orm, connect, and config

---
+++ config/orm.js
@@ -0,0 +1,84 @@
+var connection = require("./connection");
+
+// Helper function found in previous activity
+function printQuestionMarks(num) {
+   var arr = [];
+
+   for (var i = 0; i < num; i++) {
+      arr.push("?");
+   }
+
+   return arr.toString();
+}
+// Another helper function, this on to loop through the keys of the argument object and return a SQL syntaxed string
+function objToSql(ob) {
+   var arr = [];
+
+   // loop through the keys and push the key/value as a string int arr
+   for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+         // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+         if (typeof value === "string" && value.indexOf(" ") >= 0) {
+            value = "'" + value + "'";
+         }
+         // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+         // e.g. {sleepy: true} => ["sleepy=true"]
+         arr.push(key + "=" + value);
+      }
+   }
+
+   // translate array of strings to a single comma-separated string
+   return arr.toString();
+}
+
+
+var orm = {
+
+   selectAll: function (table, cb) {
+      connection.query("SELECT * FROM " + table + ";", function (err, result) {
+         if (err) throw err;
+         cb(result);
+      });
+   },
+   insertOne: function (table, col, val, cb) {
+      var queryString = "INSERT INTO " + table;
+
+      queryString += " (";
+      queryString += col.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(val.length);
+      queryString += ") ";
+
+      console.log(queryString);
+
+      connection.query(queryString, val, function (err, result) {
+         if (err) {
+            throw err;
+         }
+
+         cb(result);
+      });
+   },
+   updateOne: function (table, objColVal, condition, cb) {
+      var queryString = "UPDATE " + table;
+
+      queryString += " SET ";
+      queryString += objToSql(objColVal);
+      queryString += " WHERE ";
+      queryString += condition;
+
+      console.log(queryString);
+      connection.query(queryString, function (err, result) {
+         if (err) {
+            throw err;
+         }
+
+         cb(result);
+      });
+   }
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("BLT Burger");
+INSERT INTO burgers (burger_name) VALUES ("Wookie Burger");
+INSERT INTO burgers (burger_name) VALUES ("Quinoa Burger");
+INSERT INTO burgers (burger_name) VALUES ("Kahuna Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
