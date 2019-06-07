eaf73390401352799dcb6a102c236d6e69ab1416
Kevin Bevil <kbevil@gmail.com>
2019-06-06 20:43:00 -0400

    Progress on controllers and burger.js

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm");
+
+var burger = {
+   all: function(cb) {
+      orm.selectAll("burgers", function(res) {
+         cb(res);
+      });
+   },
+   create: function(col, val, cb) {
+      orm.insertOne("burgers", col, val, function(res) {
+         cb(res);
+      });
+   },
+   update: function(objColVal, condition, cb) {
+      orm.updateOne("burgers", objColVal, condition, function(res) {
+         cb(res);
+      });
+   }
+}
+
+module.exports = burger;
