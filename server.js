36b396cd4d2f2d621b664d6335666c053dfa693d
Kevin Bevil <kbevil@gmail.com>
2019-06-08 18:47:37 -0400

    Changes in package.json

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ var routes = require("./controllers/burgers_controllers.js");
 app.use(routes);
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res) {
-  res.json(path.join(__dirname, "public/index.html"));
-});
+
 
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
