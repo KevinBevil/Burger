842793891560da82983523a42d4962d24cad8c8a
Kevin Bevil <kbevil@gmail.com>
2019-06-10 19:59:36 -0400

    Fixing connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,6 @@ if (process.env.JAWSDB_URL) {
 
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
@@ -18,37 +17,7 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-connection.connect(function (err) {
-
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
 
-
-// Set up MySQL connection.
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "burgers_db"
-// });
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
